c95203015072637e91c788109c826390d43ee412
Joao Bosco <diasnetojoaobosco@gmail.com>
2023-02-27 15:59:49 -0300

    ini

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <HomeScreen />
+      <OlaScreen />
+      <NomeScreen />
+      <FraseScreen />
+    </View>
+    
+  );
+}
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text>Sou a HomeScreen.</Text>
+      <Image
+        source={{
+          uri: 'https://artincontext.org/wp-content/uploads/2021/02/How-to-Draw-a-Dragon.jpg'
+        }
+      }
+      style={
+        {
+          height: '300px',
+          width: '300px'
+        }
+      }
+      />
+    </View>
+  );
+}
+
+function OlaScreen() {
+  return (
+    <View>
+      <Text>Olá</Text>
+    </View>
+  );
+}
+function NomeScreen() {
+  return (
+    <View>
+      <Text>Meu nome é João Bosco</Text>
+    </View>
+  );
+}
+function FraseScreen() {
+  return (
+    <View>
+      <Text>O sonho que ispira, mas a ação que realiza</Text>
     </View>
   );
 }
